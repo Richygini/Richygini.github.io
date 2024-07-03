d82e8244d67dacc0bde28a55ec8a224f90ecec1e
RICHARD GIDEON <114780649+Richygini@users.noreply.github.com>
2024-07-04 00:03:30 +0100

    Add files via upload

---
+++ text.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="text.css">
+</head>
+<body>
+    <div class="row">
+        <div class="left">
+           <center> <br><br><br> <br><br><br> <img src="a.jpg" alt="" style="align-items: center;"></center>
+        </div>
+        <div class="right">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                    adip
+                    isicing elit. Recusandae quasi dolorum voluptatem 
+                    cumque repellat qui necessitatibus possimus, rem similique perferendis
+                     impedit aliquid maiores, vero in iusto, dolore praesentium dolorem velit?</p><p>Lorem ipsum dolor sit amet, consectetur 
+                        adip
+                        isicing elit. Recusandae quasi dolorum voluptatem 
+                        cumque repellat qui necessitatibus possimus, rem similique perferendis
+                         impedit aliquid maiores, vero in iusto, dolore praesentium dolorem velit?</p><p>Lorem ipsum dolor sit amet, consectetur 
+                            adip
+                            isicing elit. Recusandae quasi dolorum voluptatem 
+                            cumque repellat qui necessitatibus possimus, rem similique perferendis
+                             impedit aliquid maiores, vero in iusto, dolore praesentium dolorem velit?</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
